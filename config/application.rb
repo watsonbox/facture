94edd31ff542d667a8667d4e5a2be943e52793fd
Howard Wilson <howard@watsonbox.net>
2014-06-27 14:57:36 +0200

    Use new app settings for company name/description

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,5 @@ module Facture
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    config.app = ActiveSupport::OrderedOptions.new
-    config.app.company_name = 'Howard Wilson'
-    config.app.company_description = 'Freelance Software Engineer'
   end
 end
--- config/initializers/payday.rb
+++ config/initializers/payday.rb
@@ -1,4 +1,4 @@
-Payday::Config.default.company_name = Rails.configuration.app.company_name
-Payday::Config.default.company_details = Rails.configuration.app.company_description
+Payday::Config.default.company_name = Figaro.env.company_name
+Payday::Config.default.company_details = Figaro.env.company_description
 Payday::Config.default.date_format = '%b %-d, %Y'
 Payday::Config.default.renderer = Payday::ModernPdfRenderer.new

2c780a7900925867f1f7b4186e6e05fe10c14089
Howard Wilson <howard@watsonbox.net>
2014-11-19 19:10:23 +0100

    Fix JS config to work in Dokku environment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,10 @@ module ApplicationHelper
   # Outputs certain Figaro configuration vars to the client
   def javascript_configuration
     white_list = ['redmine_url', 'redmine_api_key', 'default_currency', 'reference_format']
-    javascript_tag "Facture.config = #{Figaro.env.slice(*white_list).to_json};"
+    config = white_list.each_with_object({}) do |key, hash|
+      hash[key] = Figaro.env.send(key)
+    end
+
+    javascript_tag "Facture.config = #{config.to_json};"
   end
 end
